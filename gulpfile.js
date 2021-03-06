dca9e5690017fd8f2ca5dfdca9e7e620c3f5f53b
Shatovkin Ivan <shatowkin.ivan@yandex.ru>
2021-03-07 02:54:02 +0300

    2

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,73 @@
-let preprocessor = 'sass'; 
+let preprocessor = 'scss'; 
 const { src, dest, parallel, series, watch } = require('gulp'),
     browserSync = require('browser-sync').create(),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify-es').default,
-    sass = require('gulp-sass'),
+    scss = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleancss = require('gulp-clean-css');
- 
+    cleancss = require('gulp-clean-css'),
+    del = require('del');
+
+function startwatch() {
+    watch(['src/**/*.js', '!src/**/*.min.js'], scripts);
+    watch('src/**/' + preprocessor + '/**/*', styles);
+    watch('src/**/*.html').on('change', browserSync.reload);
+    // watch('src/images/**/*', images);
+}
+
 function browsersync() {
 	browserSync.init({ 
 		server: { baseDir: 'src/' }, 
-		notify: false, 
+		notify: true, 
 		online: true 
 	})
 }
- 
+
 function scripts() {
 	return src([ 
 		'src/js/script.js', 
 		])
-	.pipe(concat('src.min.js')) 
+	.pipe(concat('script.min.js')) 
 	.pipe(uglify()) 
 	.pipe(dest('src/js/')) 
 	.pipe(browserSync.stream()) 
 }
- 
+function cleanimg() {
+	return del('app/images/dest/**/*', { force: true }) // Удаляем всё содержимое папки "app/images/dest/"
+}
 function styles() {
 	return src('src/' + preprocessor + '/styles.' + preprocessor + '')
 	.pipe(eval(preprocessor)())
-	.pipe(concat('src.min.css')) 
+	.pipe(concat('styles.min.css')) 
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 4 versions'], grid: true })) 
 	.pipe(cleancss( { level: { 1: { specialComments: 0 } } } ))
 	.pipe(dest('src/css/')) 
 	.pipe(browserSync.stream()) 
 }
  
-function images() {
-	return src('src/images/src/**/*') 
-	.pipe(dest('src/images/dest/')) 
-}
+// function images() {
+// 	return src('src/images/**/*') 
+// 	.pipe(dest('src/images/')) 
+// }
  
 function buildcopy() {
 	return src([ 
 		'src/css/**/*.min.css',
 		'src/js/**/*.min.js',
-		'src/images/dest/**/*',
+		'src/images/**/*',
 		'src/**/*.html',
 		], { base: 'src' })
-	.pipe(dest('app'))
+	.pipe(dest('dist'))
 }
- 
-// function cleandist() {
-// 	return del('dist/**/*', { force: true })
-// }
- 
-function startwatch() {
-	watch(['src/**/*.js', '!src/**/*.min.js'], scripts);
-	watch('src/**/' + preprocessor + '/**/*', styles);
-	watch('src/**/*.html').on('change', browserSync.reload);
-	watch('src/images/src/**/*', images);
+
+function cleandist() {
+	return del('dist/**/*', { force: true })
 }
  
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
-exports.images = images;
-// exports.cleanimg = cleanimg;
-exports.build = series(styles, scripts, images, buildcopy);
+// exports.images = images;
+exports.cleanimg = cleanimg;
+exports.build = series(cleandist, styles, scripts, /*images*/ buildcopy);
 exports.default = parallel(styles, scripts, browsersync, startwatch);
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('2');
+    let video = document.querySelector('.intro__video'),
+    header = document.querySelector('.header'),
+    introHeight = video.clientHeight - header.clientHeight;
+    window.addEventListener('resize', () => {
+        document.querySelector('.intro').style.height = introHeight + 'px';
+    })
+    let showMapBtn = document.querySelector('.show-map'),
+        callback = document.querySelector('.callback');
+    showMapBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        callback.classList.toggle('active');
+        if (callback.classList.contains('active')) {
+            showMapBtn.innerHTML = "Скрыть карту"
+        }
+        else {
+            showMapBtn.innerHTML = "Показать карту"
+        }
+    })
 })
