fe0bc6af52115c279b21c8b8587d01530ee06c81
Shatovkin Ivan <shatowkin.ivan@yandex.ru>
2021-03-08 03:38:46 +0300

    3

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,29 @@
+const swiper = new Swiper('.reviews-slider', {
+    slidesPerView: 3,
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+});
+
+if (window.innerWidth <= 768) {
+    const swiper = new Swiper('.about-slider', {
+        slidesPerView: 1,
+        direction: 'horizontal',
+        loop: true,
+    });
+}
 window.addEventListener('DOMContentLoaded', () => {
-    let video = document.querySelector('.intro__video'),
+    let body = document.querySelector('body'),
+    video = document.querySelector('.intro__video'),
     header = document.querySelector('.header'),
+    overlay = document.querySelector('.overlay')
     introHeight = video.clientHeight - header.clientHeight;
-    window.addEventListener('resize', () => {
         document.querySelector('.intro').style.height = introHeight + 'px';
-    })
+
     let showMapBtn = document.querySelector('.show-map'),
         callback = document.querySelector('.callback');
     showMapBtn.addEventListener('click', (e) => {
@@ -16,5 +35,25 @@ window.addEventListener('DOMContentLoaded', () => {
         else {
             showMapBtn.innerHTML = "Показать карту"
         }
+    })
+
+    let menu = document.querySelector('.menu-mobile');
+        
+        menu.addEventListener('click', () => {
+            console.log(header);
+            header.classList.add('active');
+            overlay.classList.add('active');
+        })
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('active');
+            header.classList.remove('active')
+        })
+    
+    let dropTrigger = document.querySelectorAll('.answers__item');
+    document.querySelector('.answers__item').classList.add('active');
+    dropTrigger.forEach(dropItem => {
+        dropItem.addEventListener('click', () => {
+            dropItem.classList.toggle('active');
+        })
     })
 })
